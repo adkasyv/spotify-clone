65bfc55d2f95bcb8ab320b3b8b6ca701ba26d2f7
adkasyv <kasymbekov01384@gmail.com>
2022-09-01 00:03:18 +0600

    Initial commit

---
+++ spotify_clone/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initailState } from "./utils/reducer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StateProvider initialState={initailState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>
+);
