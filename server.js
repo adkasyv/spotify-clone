5f83a863ccfa33a359b7171b91a4e5ed269ea4e5
adkasyv <kasymbekov01384@gmail.com>
2022-09-19 09:17:25 +0600

    Deploy app

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 // const favicon = require('express-favicon');
 const path = require("path");
-// const port = process.env.PORT || 3002;
-const port = 3002;
+const port = process.env.PORT || 3002;
 // const port =
 // process.env.PORT || "https://adkasyv-spotify-deploy.herokuapp.com/";
 
