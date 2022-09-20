d3fda3d8f58fb3738ab5efc4177b81b76378a6a8
adkasyv <kasymbekov01384@gmail.com>
2022-09-20 09:44:15 +0600

    Deploy app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const favicon = require('express-favicon');
 const path = require("path");
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || "3002";
 // const port =
 // process.env.PORT || "https://adkasyv-spotify-deploy.herokuapp.com/";
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 
-app.set("port", process.env.PORT || 3002);
+app.set("port", port);
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
